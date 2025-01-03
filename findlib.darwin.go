6924050e7cdd53bf04be7ef2dbd59d42b931b8d7
Adam Keys <adam@key-solutions.ca>
2025-01-03 17:49:30 -0500

    darwin: Add search paths
    This commit adds additional search paths in the findLib function. This supports python installed using newer versions of homebrew.

--- findlib.darwin.go
+++ findlib.darwin.go
@@ -8,17 +8,21 @@ import (
 )
 
 // pathPrefix is the search path prefix for finding a Python shared library on macOS systems.
-const pathPrefix = "/usr/local/Frameworks/Python.framework/Versions/Current/lib"
+var pathPrefix = []string{
+	"/opt/homebrew/Frameworks/Python.framework/Versions/Current/lib",
+	"/usr/local/Frameworks/Python.framework/Versions/Current/lib",
+}
 
 // findLib attempts to find a Python shared library on macOS systems.
 func findLib() (string, error) {
-	matches, err := filepath.Glob(filepath.Join(pathPrefix, "libpython*.dylib"))
-	if err != nil {
-		return "", fmt.Errorf("glob: %w", err)
+	for _, prefix := range pathPrefix {
+		matches, err := filepath.Glob(filepath.Join(prefix, "libpython*.dylib"))
+		if err != nil {
+			return "", fmt.Errorf("glob: %w", err)
+		}
+		if len(matches) > 0 {
+			return matches[0], nil
+		}
 	}
-	if len(matches) == 0 {
-		return "", ErrLibraryNotFound
-	}
-
-	return matches[0], nil
+	return "", ErrLibraryNotFound
 }
